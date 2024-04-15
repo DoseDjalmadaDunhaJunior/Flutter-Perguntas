7f8ef506176e9fbd5d5010d0bc5ceaca1a1f13e2
jose_djalma <devfull@p2xpay.com.br>
2024-04-15 15:37:19 -0300

    agora tem mais funções separadas

--- lib/questao.dart
+++ lib/questao.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
 class Questao extends StatelessWidget {
-  final String txt;
+  final String texto;
 
-  Questao(this.txt);
+  const Questao(this.texto, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      margin: EdgeInsets.all(10),
+      margin: const EdgeInsets.all(10),
       child: Text(
-        txt,
-        style: TextStyle(fontSize: 28),
+        texto,
+        style: const TextStyle(fontSize: 28),
         textAlign: TextAlign.center,
       ),
     );
