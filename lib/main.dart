5e8253896e17752b32b3d8d1ebd579a6f6018fcb
jose_djalma <devfull@p2xpay.com.br>
2024-04-16 09:53:06 -0300

    agora quando a acaba as perguntas ele manda uma mensagem educada

--- lib/main.dart
+++ lib/main.dart
@@ -7,44 +7,58 @@ void main() => runApp(const PerguntaApp());
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
 
+  final _perguntas = const [
+    {
+      'texto': 'Qual é a sua cor favorita?',
+      'respostas': ['azul', 'amarelo', 'vermelho', 'verde']
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': ['cachorro', 'gato', 'papagaio', 'rato']
+    },
+    {
+      'texto': 'Qual é o seu dia da semana favorito?',
+      'respostas': ['segunda', 'quarta', 'sexta', 'domingo']
+    },
+  ];
+
   void _responder() {
     setState(() {
-      _perguntaSelecionada == 2
-          ? _perguntaSelecionada = 0
-          : _perguntaSelecionada++;
+      _perguntaSelecionada++;
     });
+  }
+
+  bool get temPSelect {
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      {
-        'texto': 'Qual é a sua cor favorita?',
-        'respostas': ['azul', 'amarelo', 'vermelho', 'verde']
-      },
-      {
-        'texto': 'Qual é o seu animal favorito?',
-        'respostas': ['cachorro', 'gato', 'papagaio', 'rato']
-      },
-      {
-        'texto': 'Qual é o seu dia da semana favorito?',
-        'respostas': ['segunda', 'quarta', 'sexta', 'domingo']
-      },
-    ];
+    List<String> respostas = temPSelect
+        ? _perguntas[_perguntaSelecionada]['respostas'] as List<String>
+        : [];
+    // for (String textoResp in respostas) {
+    //   widgets.add(Resposta(textoResp, _responder));
+    // }
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Perguntas'),
         ),
-        body: Column(
-          children: [
-            Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
-            Resposta('Resposta 1', _responder),
-            Resposta('Resposta 2', _responder),
-            Resposta('Resposta 3', _responder),
-          ],
-        ),
+        body: temPSelect
+            ? Column(
+                children: [
+                  Questao(_perguntas[_perguntaSelecionada]['texto'].toString()),
+                  ...respostas.map((t) => Resposta(t, _responder)),
+                ],
+              )
+            : Center(
+                child: Text(
+                  'Vai se foder',
+                  style: TextStyle(fontSize: 28),
+                ),
+              ),
       ),
     );
   }
